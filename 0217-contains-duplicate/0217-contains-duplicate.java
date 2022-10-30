89b36ebc74ddef2f08a395c1f89e5c59830ef5b4
Amey Dhuri <88235763+SayuriYuto@users.noreply.github.com>
2022-10-30 19:41:42 +0530

    Time: 17 ms (67.19%), Space: 71.3 MB (22.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,5 +1,5 @@
 class Solution {
-
+    // I tried with array list but it resulted in TLE but apparently set uses lesser time complexity
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> uniques = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
