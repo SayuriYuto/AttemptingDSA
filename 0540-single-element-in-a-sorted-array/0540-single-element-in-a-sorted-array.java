9be02c5bb24e48a60dd93163ab4ff06a8acbbe15
Amey Dhuri <88235763+SayuriYuto@users.noreply.github.com>
2023-02-21 19:27:46 +0530

    Time: 0 ms (100.00%), Space: 48.1 MB (72.85%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,7 +3,6 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
         int left = 0;
         int right = nums.length-1;
-        int mid = left+(right-left)/2;
         unique = 0;
         solve(left,right,nums);
         return unique;
@@ -14,16 +13,16 @@ class Solution {
             unique = nums[start];
             return;
         }
-        if(end-start+1==3){
-            if(nums[mid] == nums[mid-1]){
-                unique = nums[mid+1];
-                return;
-            }
-            else{
-                unique = nums[mid-1];
-                return;
-            }
-        }
+        // else if(end-start+1==3){
+        //     if(nums[mid] == nums[mid-1]){
+        //         unique = nums[mid+1];
+        //         return;
+        //     }
+        //     else{
+        //         unique = nums[mid-1];
+        //         return;
+        //     }
+        // }
         
         int start1 = start, start2 = start,end1 = end,end2 = end;
         
