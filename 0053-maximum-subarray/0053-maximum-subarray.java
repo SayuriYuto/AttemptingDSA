ab4aa14ff49d0574c0e9046995717b0ccd18f974
Amey Dhuri <88235763+SayuriYuto@users.noreply.github.com>
2023-03-14 19:07:31 +0530

    Time: 2 ms (20.36%), Space: 51.9 MB (28.69%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int maxSubArray = nums[0];
-        int cursum = 0;
-        for(int i=0;i<nums.length;i++){
-            if(cursum<0) cursum = 0; // if the sum goes negative reset to 0 and start finding new subarray with greater positive sum
-            cursum+=nums[i];
-            maxSubArray = Math.max(cursum,maxSubArray);
+        int curSum = 0;
+        for(int i =0;i<nums.length;i++){
+            if(curSum<0) curSum = 0;
+            curSum+=nums[i];
+            maxSubArray = Math.max(maxSubArray, curSum);
         }
         return maxSubArray;
-        
     }
 }
